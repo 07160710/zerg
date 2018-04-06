efbf663ddfcb319f97b973e729074aeefb6b5e8e
huangkaifeng <1195745286@qq.com>
2018-04-06 11:35:05 +0800

    虚拟主机的配置及url的简化

---
+++ application/sample/controller/Test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/6
+ * Time: 10:47
+ */
+
+namespace app\sample\controller;
+
+
+class Test
+{
+    //http://z.cn/sample/test/hello
+    //localhost/zerg/public/index.php = z.cn
+    //D:\xampp\apache\conf\extra
+    //C:\Windows\system32\drivers\etc\hosts
+    public function hello()
+    {
+        return 'Hello,KAIFENG';
+    }
+}

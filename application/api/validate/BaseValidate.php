0f4b9c6a2d389fd28872a4a626d7e2b26409b3c3
huangkaifeng <1195745286@qq.com>
2018-04-08 19:44:26 +0800

    全局异常处理

--- application/api/validate/BaseValidate.php
+++ application/api/validate/BaseValidate.php
@@ -19,13 +19,13 @@ class BaseValidate extends Validate
     public function goCheck(){
         $request = Request::instance();
         $params = $request->param();
-        $result = $this->check($params);
+
+        $result = $this->batch()->check($params);
         if(!$result){
-            $e = new ParameterException();
-            $e->msg =$this->error;
+            $e = new ParameterException([
+                'msg' => $this->error
+            ]);
             throw $e;
-//            $error = $this->error;
-//            throw new Exception($error);
         }
         else{
             return true;
--- application/api/validate/IDMustBePositiveInt.php
+++ application/api/validate/IDMustBePositiveInt.php
@@ -12,7 +12,8 @@ use think\Validate;
 class IDMustBePositiveInt extends BaseValidate
 {
     protected $rule = [
-        'id' => 'require|isPositiveInteger'
+        'id' => 'require|isPositiveInteger',
+        'num' => 'in:1,2,3'
     ];
     protected function isPositiveInteger( $value, $rule = '', $data = '', $field = ''){
         if(is_numeric($value) && is_int($value + 0) && ($value + 0) > 0){

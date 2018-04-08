4ab68d1e3552da2a4d8c254887db758fbe129952
huangkaifeng <1195745286@qq.com>
2018-04-08 16:39:39 +0800

    自定义日志...

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -12,6 +12,7 @@ use app\api\validate\IDMustBePositiveInt;
 
 use app\api\model\Banner as BannerModel;
 use app\lib\exception\BannerMissException;
+use think\Exception;
 
 class Banner
 {
@@ -22,7 +23,8 @@ class Banner
 
         $banner = BannerModel::getBannerByID($id);
         if(!$banner){
-            throw new BannerMissException();
+            //throw new BannerMissException();
+            throw new Exception('内部错误');
         }
         return $banner;
     }
--- application/lib/exception/ExceptionHandler.php
+++ application/lib/exception/ExceptionHandler.php
@@ -11,6 +11,7 @@ namespace app\lib\exception;
 
 use think\Exception;
 use think\exception\Handle;
+use think\Log;
 use think\Request;
 
 class ExceptionHandler extends Handle
@@ -29,6 +30,7 @@ class ExceptionHandler extends Handle
             $this->code = 500;
             $this->msg = '服务器内部错误，不想告诉你';
             $this->errorCode = 999;
+            $this->recordErrorLog($e);
         }
         $request = Request::instance();
         $result = [
@@ -37,5 +39,13 @@ class ExceptionHandler extends Handle
             'request_url' => $request->url()
         ];
         return json($result,$this->code);
+    }
+    private function recordErrorLog(Exception $e){
+        Log::init([
+           'type' => 'File',
+           'path' => LOG_PATH,
+           'level' => ['error']
+        ]);
+        Log::record($e->getMessage(),'error');
     }
 }

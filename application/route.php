acc3624d82da0c11effbef7de325346e9feb266e
huangkaifeng <1195745286@qq.com>
2018-04-14 14:06:48 +0800

    部分修改

--- application/route.php
+++ application/route.php
@@ -25,8 +25,18 @@ use think\Route;
 Route::get('api/:version/banner/:id','api/:version.Banner/getBanner');
 Route::get('api/:version/theme','api/:version.Theme/getSimpleList');
 Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
-Route::get('api/:version/product/recent','api/:version.Product/getRecent');
+
+
 Route::get('api/:version/product/by_category','api/:version.Product/getAllInCategory');
-Route::get('api/:version/product/:id','api/:version.Product/getOne');
+Route::get('api/:version/product/:id','api/:version.Product/getOne',[],['id'=>'\d+']);
+Route::get('api/:version/product/recent','api/:version.Product/getRecent');
+
+//Route::group('api/:version/product',function (){
+//    Route::get('/by_category','api/:version.Product/getAllInCategory');
+//    Route::get('/:id','api/:version.Product/getOne',[],['id'=>'\d+']);
+//    Route::get('/recent','api/:version.Product/getRecent');
+//});
+
+
 Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
 Route::post('api/:version/token/user','api/:version.Token/getToken');
